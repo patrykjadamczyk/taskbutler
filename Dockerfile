fa65dbae3f811034470ce17781f78381aaa5c48e
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2020-09-26 00:19:48 +0000

    Added Update Sync Timeout Environment Variable

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ COPY . /app
 WORKDIR /app
 RUN pip install .
 RUN mkdir /root/.taskbutler
+ENV SYNC_TIMEOUT="1m"
 VOLUME /root/.taskbutler
 CMD ["/app/docker/docker-entrypoint.sh"]
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 chmod 777 /root/.taskbutler
+echo "Timeout was set to" ${SYNC_TIMEOUT:-"1m"}
 while true; do
     taskbutler
-    sleep 1m
+    sleep ${SYNC_TIMEOUT:-"1m"}
 done
